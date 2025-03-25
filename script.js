7a8dca6c8f746b74868c2958fe06c337747fe1c8
Trayp16 <xKingRavenx20@gmail.com>
2025-03-25 08:59:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+console.log("Welcome to my JavaScript Page!");
+console.log("JavaScript makes webpages interactive!");
+
+alert("Hello! Welcome to my webpage.");
+
+document.getElementById("title").textContent = "Welcome to my JavaScript Page";
+document.getElementById("message").textContent = "This message was added using ";
